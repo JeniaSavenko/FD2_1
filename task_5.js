cf8dbe071d1e3d13a3f19bd5c7e43de476d1f5d6
JeniaSavenko <34649508+jeniasavenko@users.noreply.github.com>
2018-03-19 16:55:25 +0300

    fix task 5-6

--- task_5.js
+++ task_5.js
@@ -2,14 +2,32 @@ var arr = [];
 for (var i = 0; i < 7; i++)
   arr.push(Math.floor(Math.random()*(10-1+1))+1);
 console.log(arr);
-var reversArr = arr.reverse();
-console.log(reversArr);
 
+(function () {
+  let len = (arr.length-1)/2;
+  for (var i = 0; i<len; ++i){
+    arr.push(arr[0]);
+    arr.splice(0, 1);
+  }
+  arr.splice(len+1, 0, arr[0]);
+  arr.splice(0, 1);
+  console.log(arr);
+})();
 
 
 var arr2 = [];
 for (var i = 0; i < 9; i++)
   arr2.push(Math.floor(Math.random()*(10-1+1))+1);
 console.log(arr2);
-var reversArr2 = arr2.reverse();
-console.log(reversArr2);
+
+(function () {
+  let len = (arr2.length-1)/2;
+  for (var i = 0; i<len; ++i){
+    arr2.push(arr2[0]);
+    arr2.splice(0, 1);
+  }
+  arr2.splice(len+1, 0, arr2[0]);
+  arr2.splice(0, 1);
+  console.log(arr2);
+})();
+
